328a7006e24aef51196ba680ada169cd4677e9fe
David Cook <divergentdave@gmail.com>
2023-02-04 18:00:50 -0600

    Atomically create GOQUE file

--- file.go
+++ file.go
@@ -3,6 +3,7 @@ package goque
 import (
 	"os"
 	"path/filepath"
+	"syscall"
 )
 
 // goqueType defines the type of Goque data structure used.
@@ -35,17 +36,38 @@ func checkGoqueType(dataDir string, gt goqueType) (bool, error) {
 	// Read 'GOQUE' file for this directory.
 	f, err := os.OpenFile(path, os.O_RDONLY, 0)
 	if os.IsNotExist(err) {
-		f, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0644)
+		// Write the metadata to a temporary file first.
+		tempPath := filepath.Join(dataDir, "GOQUE.tmp")
+		f, err = os.OpenFile(tempPath, os.O_RDWR|os.O_CREATE, 0644)
 		if err != nil {
 			return false, err
 		}
-		defer f.Close()
 
 		// Create byte slice of goqueType.
 		gtb := make([]byte, 1)
 		gtb[0] = byte(gt)
 
 		_, err = f.Write(gtb)
+		if err != nil {
+			f.Close()
+			return false, err
+		}
+
+		err = f.Sync()
+		if err != nil {
+			f.Close()
+			return false, err
+		}
+		err = f.Close()
+		if err != nil {
+			return false, err
+		}
+		// Atomically swap the file into place.
+		err = os.Rename(tempPath, path)
+		if err != nil {
+			return false, err
+		}
+		err = syncDir(dataDir)
 		if err != nil {
 			return false, err
 		}
@@ -77,4 +99,29 @@ func checkGoqueType(dataDir string, gt goqueType) (bool, error) {
 	}
 
 	return false, nil
+}
+
+// syncDir calls fsync(2) on a directory, which is necessary on Linux, and
+// ignores EINVAL errors that may arise on other systems.
+func syncDir(path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if err := f.Sync(); err != nil {
+		if err == os.ErrInvalid {
+			return nil
+		}
+		// Go < 1.8
+		if syserr, ok := err.(*os.SyscallError); ok && syserr.Err == syscall.EINVAL {
+			return nil
+		}
+		// Go >= 1.8
+		if patherr, ok := err.(*os.PathError); ok && patherr.Err == syscall.EINVAL {
+			return nil
+		}
+		return err
+	}
+	return nil
 }
